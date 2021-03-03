575399519e30a46ff1443312ca2d4b2e4d55beb7
Levic559 <ychen559@my.bcit.ca>
2021-03-03 21:13:34 +0800

    first commit

---
+++ comps/BtnStart/index.js
@@ -0,0 +1,19 @@
+function BtnStartUI(){
+  return`
+  <div style=" 
+  width:175px;
+  height:55px;
+  border-radius: 27.5px;
+  font-size:20px;
+  line-height: 55px;
+  text-align: center;
+  font-weight:600;
+  font-family: Roboto;
+  background-color:#C4C4C4;
+  color:#454545;
+  ">Let's start</div>
+  
+  `
+}
+
+export  const BtnStart = BtnStartUI();
---
+++ comps/Logo/index.js
@@ -0,0 +1,14 @@
+function LogoUI(){
+  return`
+  <div style=" 
+  font-size:84px;
+  font-weight:700;
+  font-family: Roboto;
+  color:#E95719;
+  text-shadow:3px 3px 3px grey ;
+  ">BIRD</div>
+  
+  `
+}
+
+export  const Logo = LogoUI();
