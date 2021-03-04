3f3efeac97563375ef11e12c2f8c70e01abf1b06
Levic559 <ychen559@my.bcit.ca>
2021-03-04 13:24:39 +0800

    third commit

--- comps/Logo/index.js
+++ comps/Logo/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function LogoUI(fontSize="84",fontWeight="700"){
   return`
   <div style=" 
@@ -31,38 +30,5 @@ LogoUI.Handleclick = (el) =>{
   el.style.top = "-20px";
   el.style.fontSize="550%";
 }
-=======
-function LogoUI(fontSize="84",fontWeight="700"){
-  return`
-  <div style=" 
-  font-size:${fontSize}px;
-  font-weight:${fontWeight};
-  font-family: Roboto;
-  position:absolute;
-  color:#E95719;
-  text-shadow:3px 3px 3px grey ;
-  transition: opactity 0.3s; left 0.5s;
-  opacity:1;
-  "
-  onmouseover="LogoUI.HandleMouseover(this)";
-  onmouseout="LogoUI.HandleMouseout(this)";
-  onclick="LogoUI.Handleclick(this)";
-  >BIRD</div>
-  
-  `
-}
 
-LogoUI.HandleMouseover = (el) =>{
-  el.style.opacity = 0.75;
-}
-
-LogoUI.HandleMouseout = (el) =>{
-  el.style.opacity = 1;
-}
-
-LogoUI.Handleclick = (el) =>{
-  el.style.top = "-20px";
-  el.style.fontSize="550%";
-}
->>>>>>> a0be75d4adde31d731bcd513b91d0ca471d00cb6
 // export  const Logo = LogoUI();
--- stories/MyComps.stories.js
+++ stories/MyComps.stories.js
@@ -2,19 +2,15 @@ import {Logo} from '../comps/Logo';
 import {Banner} from '../comps/Banner';
 import {BtnStart} from '../comps/BtnStart';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> a0be75d4adde31d731bcd513b91d0ca471d00cb6
 export default {
   title: 'My Components'
 }
 
-<<<<<<< HEAD
+
 // export const MyLogo = () => Logo;
-=======
+
 export const MyLogo = () => Logo;
->>>>>>> a0be75d4adde31d731bcd513b91d0ca471d00cb6
+
 export const MyBanner = () => Banner;
 export const MyBtnStart = () => BtnStart;
 // export const MyUsers = () => Users;
