a0be75d4adde31d731bcd513b91d0ca471d00cb6
Levic559 <72251069+Levic559@users.noreply.github.com>
2021-03-04 13:00:48 +0800

    Add files via upload

--- comps/Logo/index.js
+++ comps/Logo/index.js
@@ -1,14 +1,33 @@
-function LogoUI(){
+function LogoUI(fontSize="84",fontWeight="700"){
   return`
   <div style=" 
-  font-size:84px;
-  font-weight:700;
+  font-size:${fontSize}px;
+  font-weight:${fontWeight};
   font-family: Roboto;
+  position:absolute;
   color:#E95719;
   text-shadow:3px 3px 3px grey ;
-  ">BIRD</div>
+  transition: opactity 0.3s; left 0.5s;
+  opacity:1;
+  "
+  onmouseover="LogoUI.HandleMouseover(this)";
+  onmouseout="LogoUI.HandleMouseout(this)";
+  onclick="LogoUI.Handleclick(this)";
+  >BIRD</div>
   
   `
 }
 
-export  const Logo = LogoUI();
+LogoUI.HandleMouseover = (el) =>{
+  el.style.opacity = 0.75;
+}
+
+LogoUI.HandleMouseout = (el) =>{
+  el.style.opacity = 1;
+}
+
+LogoUI.Handleclick = (el) =>{
+  el.style.top = "-20px";
+  el.style.fontSize="550%";
+}
+// export  const Logo = LogoUI();
