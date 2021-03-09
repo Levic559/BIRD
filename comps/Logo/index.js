a271543a5820761b96dd38e04661caa71a4a2484
Levic559 <ychen559@my.bcit.ca>
2021-03-10 04:32:21 +0800

    layout-1

--- comps/Logo/index.js
+++ comps/Logo/index.js
@@ -1,4 +1,4 @@
-function LogoUI(fontSize="84",fontWeight="700"){
+function LogoUI(fontSize="84",fontWeight="700", Scolor="grey"){
   return`
   <div style=" 
   font-size:${fontSize}px;
@@ -6,7 +6,7 @@ function LogoUI(fontSize="84",fontWeight="700"){
   font-family: Roboto;
   position:absolute;
   color:#E95719;
-  text-shadow:3px 3px 3px grey ;
+  text-shadow:3px 3px 3px ${Scolor} ;
   transition: opactity 0.3s; left 0.5s;
   opacity:1; 
   onmouseover="LogoUI.HandleMouseover(this)";
