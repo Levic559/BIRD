8cb4f3dc82f5a076121c6be94065ada0358f6ce2
AdrianTejada <atejada@my.bcit.ca>
2021-03-10 03:44:40 -0800

    finished General info4-6, just need to add interactions for 6

--- .history/General_info-6/2_20210310033003.js
+++ .history/General_info-6/2_20210310033003.js
@@ -1,3 +1,3 @@
 document.querySelector(".header").innerHTML += LogoUI(30,700, "#C4C4C4");
 document.querySelector(".banner").innerHTML += BannerUI("General Info")
-document.querySelector(".BtnPages").innerHTML += BtnPagesUI("#FF7A00", "#FF7A00", "../General_info-4/3.html", "../General_info-6/1.html" );
+document.querySelector(".BtnPages").innerHTML += BtnPagesUI("#FF7A00", "#FF7A00", "1.html", "3.html" );
